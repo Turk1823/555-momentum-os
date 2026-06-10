0e69184444f29d94bb30e1de5d06d837746c1f01
Linkon Axon <trexinol@hotmail.co.uk>
2026-06-10 13:14:22 +0100

    Add MomentumOS logo across diagnostic app

--- components/momentumos-logo.tsx
+++ components/momentumos-logo.tsx
@@ -6,13 +6,15 @@ type MomentumOSLogoProps = {
 };
 
 export function MomentumOSLogo({ className = "", width = 220 }: MomentumOSLogoProps) {
+  const height = Math.round((width / 1920) * 450);
+
   return (
     <Image
       alt="MomentumOS"
       className={className}
-      height={48}
+      height={height}
       priority
-      src="/momentumos-logo.svg"
+      src="/momentumos-logo.png"
       width={width}
     />
   );
