23a588eb25ca778d23551bb5ee3d8b282410249f
Linkon Axon <trexinol@hotmail.co.uk>
2026-06-12 22:47:02 +0100

    Adjust diagnostic logo left alignment

--- components/momentumos-logo.tsx
+++ components/momentumos-logo.tsx
@@ -12,7 +12,7 @@ export function MomentumOSLogo({ className = "", width = 220 }: MomentumOSLogoPr
   return (
     <div
       className={cn("relative inline-block shrink-0", className)}
-      style={{ width: `${width}px`, marginLeft: "-12px" }}
+      style={{ width: `${width}px`, marginLeft: "-24px" }}
     >
       <Image
         alt="MomentumOS"
